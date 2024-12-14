9a58233a9181feb3ebd66ed5c2c328c36a57de54
aldoprogrammer <aldobesma@gmail.com>
2024-12-14 18:42:47 +0700

    submit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Multi-Chain-Payment-Gateway
+
+This project sets up a Vite-React TypeScript application and integrates it with the Wormhole Connect SDK.
+
+## Prerequisites
+
+Ensure you have the following installed on your system:
+
+- **Node.js** & **TypeScript**
+- **npm** or **yarn**
+
+## Setup
+
+### 1. Clone the Repository
+
+First, clone the repository to your local machine:
+
+```bash
+git clone https://github.com/aldoprogrammer/Multi-Chain-Payment-Gateway.git
+cd Multi-Chain-Payment-Gateway
+```
+
+### 2. Download Dependencies
+
+Make sure to install all required dependencies using `npm` or `yarn`:
+
+```bash
+# Using npm
+npm install
+
+# Or using yarn
+yarn
+```
+```
+
+### 3. Adjust WormholeConnectConfig
+
+Adjust the `WormholeConnectConfig` in `App.tsx` based on the `deployment.json` file from your NTT deployment. This configuration is essential to ensure proper integration with your deployment environment.
+
+### 4. Run the App
+
+Finally, run your application:
+
+```bash
+yarn dev
+```
+
+or, if using npm:
+
+```bash
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import WormholeConnect, {
+    WormholeConnectConfig,
+    nttRoutes,
+    WormholeConnectTheme,
+  } from '@wormhole-foundation/wormhole-connect';
+  
+  const wormholeConfig: WormholeConnectConfig = {
+    network: 'Testnet',
+    chains: ['Sepolia', 'Solana', 'BaseSepolia'],
+    tokens: ['WSVsep', 'WSVsol', 'WSVbase'],
+    ui: {
+      title: 'Multi-Chain Payment Gateway',
+      defaultInputs: {
+        fromChain: 'Sepolia',
+        toChain: 'Solana'
+      },
+      showHamburgerMenu: false,
+    },
+    routes: [
+      ...nttRoutes({
+        tokens: {
+          WSV_NTT: [
+            {
+              chain: 'Sepolia',
+              manager: '0xD1542431f7da242Beebb72F283810fd6A40c62F7',
+              token: '0xAdB9299DCeAc8440844Ee1C980bf7F4fCc26780A',
+              transceiver: [
+                {
+                  address: '0x4C4e885E273e162b81a7D842521Eb1C9342c1d30',
+                  type: 'wormhole',
+                },
+              ],
+            },
+            {
+              chain: 'Solana',
+              manager: 'nMxHx1o8GUg2pv99y8JAQb5RyWNqDWixbxWCaBcurQx',
+              token: '2vLDzr7hUpLFHQotmR8EPcMTWczZUwCK31aefAzumkmv',
+              transceiver: [
+                {
+                  address: 'AjL3f9FMHJ8VkNUHZqLYxa5aFy3aTN6LUWMv4qmdf5PN',
+                  type: 'wormhole',
+                },
+              ],
+            },
+            {
+              chain: 'BaseSepolia',
+              manager: '0xaE02Ff9C3781C5BA295c522fB469B87Dc5EE9205',
+              token: '0xb8dccDA8C166172159F029eb003d5479687452bD',
+              transceiver: [
+                {
+                  address: '0xF4Af1Eac8995766b54210b179A837E3D59a9F146',
+                  type: 'wormhole',
+                },
+              ],
+            },
+          ],
+        },
+      }),
+    ],
+    tokensConfig: {
+      WSVsep: {
+        key: 'WSVsep',
+        symbol: 'WSV',
+        nativeChain: 'Sepolia',
+        displayName: 'WSV',
+        tokenId: {
+          chain: 'Sepolia',
+          address: '0xAdB9299DCeAc8440844Ee1C980bf7F4fCc26780A'
+        },
+        coinGeckoId: 'wormhole',
+        icon: 'https://wormhole.com/token.png',
+        decimals: 18
+      },
+      WSVsol: {
+        key: 'WSVsol',
+        symbol: 'WSV',
+        nativeChain: 'Solana',
+        displayName: 'WSV',
+        tokenId: {
+          chain: 'Solana',
+          address: '2vLDzr7hUpLFHQotmR8EPcMTWczZUwCK31aefAzumkmv'
+        },
+        coinGeckoId: 'wormhole',
+        icon: 'https://wormhole.com/token.png',
+        decimals: 9
+      },
+      WSVbase: {
+        key: 'WSVbase',
+        symbol: 'WSV',
+        nativeChain: 'BaseSepolia',
+        displayName: 'WSV',
+        tokenId: {
+          chain: 'BaseSepolia',
+          address: '0xb8dccDA8C166172159F029eb003d5479687452bD'
+        },
+        coinGeckoId: 'wormhole',
+        icon: 'https://wormhole.com/token.png',
+        decimals: 9
+      }
+    }
+  }
+  
+  function App() {
+    const theme: WormholeConnectTheme = {
+      mode: 'dark',
+      primary: '#78c4b6',
+    };
+
+    return (
+      <div>
+        <WormholeConnect config={wormholeConfig} theme={theme} />
+      </div>
+    )
+  }
+  export default App
